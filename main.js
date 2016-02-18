aeb69e9311d98d850015ded86f069b538dbb53eb
penguinfreud <qiyiwsy@yahoo.com>
2016-02-18 21:51:07 +0800

    added conflict solving

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function parseArgs() {
 }
 
 function usage() {
-	console.log("node main.js [-c <config file>] [-u id] [-p password] [-i interval] [-s <server no.>] [-e <course1>,<course2>,...]");
+	console.log("node main.js [-c <config file>] [-u <id>] [-p <password>] [-i <check interval>] [-s <server no.>] [-e <course1>,<course2>,...]");
 	process.exit();
 }
 
@@ -50,11 +50,16 @@ function validate(options) {
 		return false;
 	}
 	if (options.interval === -1) {
-		options.interval = 600;
+		options.interval = 250;
 	} else if (options.interval <= 0) {
-		console.log("invalid interval: " + interval);
+		console.log("invalid interval: " + options.interval);
 		return false;
 	}
+	if (options.interval < 200) {
+		console.log("warning: interval " + options.interval + " may be too short");
+	}
+	if (!options.courses) options.courses = [];
+	if (!options.conflicts) options.conflicts = [];
 	return true;
 }
 
@@ -75,7 +80,6 @@ function getOptions() {
 			options[key] = args[key];
 		}
 	}
-	validate(options) || process.exit();
 }
 
 var options = {
@@ -83,15 +87,16 @@ var options = {
 	user: null,
 	password: null,
 	courses: null,
+	conflicts: null,
 	server: 1
 },
 sessionId = null,
-blocking = 0,
+loggingIn = false,
 loggedIn = false,
 courseData,
 lessonCounts,
 electedCourses,
-byId, byNo;
+wantedCourses;
 
 exports.options = options;
 
@@ -100,15 +105,7 @@ exports.getSessionId = function () {
 };
 
 exports.reset = function () {
-	blocking = 0;
-};
-
-exports.getCourseData = function () {
-	return courseData;
-};
-
-exports.getLessonCounts = function () {
-	return lessonCounts;
+	loggingIn = false;
 };
 
 function prepareHeaders(headers) {
@@ -122,8 +119,8 @@ function prepareHeaders(headers) {
 	}
 }
 
-function request(obj, cb, fail) {
-	++blocking;
+function request(obj, cb, fail, isLogin) {
+	if (isLogin) loggingIn = true;
 	
 	prepareHeaders(obj.headers);
 	
@@ -133,7 +130,7 @@ function request(obj, cb, fail) {
 		console.log("Error");
 		console.log(new Error().stack);
 		fail && process.nextTick(fail);
-		--blocking;
+		if (isLogin) loggingIn = false;
 	});
 	
 	req.on("response", function (res) {
@@ -149,10 +146,10 @@ function request(obj, cb, fail) {
 		if (res.statusCode === 302 && res.headers.location === "http://xk.fudan.edu.cn/xk/login.action") {
 			login();
 			fail && process.nextTick(fail);
-			--blocking;
+			if (isLogin) loggingIn = false;
 		} else {
 			cb(res);
-			--blocking;
+			if (isLogin) loggingIn = false;
 		}
 	});
 	
@@ -160,7 +157,7 @@ function request(obj, cb, fail) {
 }
 
 function login(succeed, fail) {
-	if (blocking > 0) return;
+	if (loggingIn) return;
 	console.log("login");
 	loggedIn = false;
 	
@@ -184,7 +181,7 @@ function login(succeed, fail) {
 			console.log("login failed");
 			fail && process.nextTick(fail);
 		}
-	}, fail);
+	}, fail, true);
 	
 	req.write(body);
 	req.end();
@@ -220,7 +217,7 @@ function loginPhase3(succeed, fail) {
 		});
 		res.on("end", function () {
 			var i = 0, j, l = data.length;
-			electedCourses = [];
+			exports.electedCourses = electedCourses = [];
 			while (true) {
 				i = data.indexOf("electedIds[\"l", i+1);
 				if (i === -1) break;
@@ -237,7 +234,7 @@ function loginPhase3(succeed, fail) {
 }
 
 function loadData(succeed) {
-	if (blocking > 0) return;
+	if (loggingIn) return;
 	request({
 		method: "GET",
 		hostname: "xk.fudan.edu.cn",
@@ -251,23 +248,25 @@ function loadData(succeed) {
 			content += chunk.toString("UTF-8");
 		});
 		res.on("end", function () {
-			courseData = eval(content.substr(content.indexOf("[")));
+			exports.courseData = courseData = eval(content.substr(content.indexOf("[")));
+			console.log("data loaded");
 			prepareData();
 			convertElectedCourses();
-			console.log("data loaded");
-			succeed && process.nextTick(succeed);
+			convertWantedCourses();
+			checkConflict();
+			succeed && succeed();
 		});
 	}, function () {
-		loadData(success);
+		loadData(succeed);
 	}).end();
 }
 
 exports.loadData = loadData;
 
 function prepareData() {
+	exports.byId = byId = {};
+	exports.byNo = byNo = {};
 	var i, course;
-	byId = {};
-	byNo = {};
 	for (i = 0; i<courseData.length; i++) {
 		course = courseData[i];
 		byId[course.id] = course;
@@ -277,15 +276,33 @@ function prepareData() {
 
 function convertElectedCourses() {
 	if (courseData && electedCourses.length > 0 && typeof electedCourses[0] === "number") {
-		var i;
+		var i, course;
 		for (i = 0; i<electedCourses.length; i++) {
-			electedCourses[i] = byId[electedCourses[i]];
+			course = electedCourses[i] = byId[electedCourses[i]];
+			course.elected = true;
+			console.log("elected: " + course.no + " " + course.name);
+		}
+	}
+}
+
+function convertWantedCourses() {
+	if (courseData) {
+		var i, course, no;
+		exports.wantedCourses = wantedCourses = [];
+		for (i = 0; i<options.courses.length; i++) {
+			course = byNo[options.courses[i]];
+			if (course) {
+				wantedCourses.push(course);
+				console.log("added to list: " + course.no + " " + course.name);
+			} else {
+				console.log("course not found: " + options.courses[i]);
+			}
 		}
 	}
 }
 
 function loadCounts(succeed) {
-	if (blocking > 0) return;
+	if (loggingIn) return;
 	request({
 		method: "GET",
 		 hostname: "xk.fudan.edu.cn",
@@ -299,22 +316,54 @@ function loadCounts(succeed) {
 			content += chunk.toString("UTF-8");
 		});
 		res.on("end", function () {
-			lessonCounts = eval("(" + content.substr(content.indexOf("{")) + ")");
-			process.stdout.write("@");
-			succeed && process.nextTick(succeed);
+			if (content.substr(0, 4) == "/*sc") {
+				exports.lessonCounts = lessonCounts = eval("(" + content.substr(content.indexOf("{")) + ")");
+				process.stdout.write("@");
+				triggerPollEvent();
+				succeed && succeed();
+			} else {
+				process.stdout.write("!");
+			}
 		});
 	}, function () {
-		loadCounts(success);
+		loadCounts(succeed);
 	}).end();
 }
 
 exports.loadCounts = loadCounts;
 
-function elect(course, isQuit) {
-	if (blocking) return;
-	var body = isQuit?
-		"optype=false&operator0=" + course.id + "%3Afalse":
-		"optype=true&operator0=" + course.id + "%3Atrue%3A0";
+var pollListeners = exports.pollListeners = [];
+
+function triggerPollEvent() {
+	var i;
+	for (i = 0; i<pollListeners.length; i++) {
+		pollListeners[i]();
+	}
+}
+
+var Enum = 0;
+var ELECTING = ++Enum,
+ELECTED = ++Enum,
+DROPPING = ++Enum,
+UNELECTED = ++Enum;
+
+function resetState(course) {
+	course.state = course.elected? ELECTED: UNELECTED;
+}
+
+function elect(course, isDrop, succeed, fail) {
+	if (!course.state) resetState(course);
+	if (loggingIn || course.state !== (isDrop? ELECTED: UNELECTED)) return;
+	
+	var body = isDrop?
+	"optype=false&operator0=" + course.id + "%3Afalse":
+	"optype=true&operator0=" + course.id + "%3Atrue%3A0";
+	
+	course.state = ELECTING;
+	
+	//console.log((isDrop? "dropping ": "electing ") + course.no + " " + course.name);
+	process.stdout.write(".");
+	
 	var req = request({
 		method: "POST",
 		host: "xk.fudan.edu.cn",
@@ -332,14 +381,43 @@ function elect(course, isQuit) {
 				data += chunk.toString();
 			});
 			res.on("end", function () {
+				var message = (data.match(/<div[^>]*>((?:.|[\r\n])*)<\/div>/m) || [,""])[1];
 				if (data.indexOf("success") >= 0) {
-					console.log("\nelected " + course.no + " " + course.name);
-					courses.splice(courses.indexOf(course.id), 1);
-				} else if (data.indexOf("Please DO NOT click too quickly.") >= 0) {
-					console.log("\ntoo quick");
+					if (isDrop) {
+						console.log("\ndropped " + course.no + " " + course.name);
+						course.elected = false;
+						course.state = UNELECTED;
+						electedCourses.splice(electedCourses.indexOf(course), 1);
+					} else {
+						console.log("\nelected " + course.no + " " + course.name);
+						course.elected = true;
+						course.state = ELECTED;
+						electedCourses.push(course);
+					}
+					succeed && succeed();
+				} else if (data.indexOf("Please DO NOT click too quickly.") >= 0 ||
+					data.indexOf("服务器内部错误") >= 0) {
+					process.stdout.write("*");
+					resetState(course);
+					fail && fail();
+				} else if (message.indexOf("公选人数已满") >= 0) {
+					process.stdout.write("|");
+					resetState(course);
+					fail && fail();
+				} else if (data.indexOf("failure") >= 0) {
+					resetState(course);
+					console.log(message);
+					fail && fail();
+				} else {
+					resetState(course);
+					console.log(message);
+					fail && fail();
 				}
 			});
 		}
+	}, function () {
+		resetState(course);
+		fail();
 	});
 	
 	req.write(body);
@@ -348,18 +426,170 @@ function elect(course, isQuit) {
 
 exports.elect = elect;
 
-function main() {
-	console.log("main");
-	getOptions();
-	console.log(options);
-	login(function () {
-		loadData(function () {
-			loadCounts();
-		});
-	});
+function timeConflict(a, b) {
+	var units = {}, i, o, j;
+	for (i = 0; i < a.arrangeInfo.length; i++) {
+		o = a.arrangeInfo[i];
+		for (j = o.startUnit; j<o.endUnit; j++) {
+			units[o.weekDay * 15 + j] = 1;
+		}
+	}
+	
+	for (i = 0; i < b.arrangeInfo.length; i++) {
+		o = b.arrangeInfo[i];
+		for (j = o.startUnit; j<o.endUnit; j++) {
+			if (units[o.weekDay * 15 + j] === 1) {
+				return true;
+			}
+		}
+	}
+	
+	return false;
 }
 
-exports.main = main;
+exports.timeConflict = timeConflict;
+
+function userDefinedConflict(a, b) {
+	var i, con;
+	for (i = 0; i<options.conflicts; i++) {
+		con = options.conflicts[i];
+		if (con.indexOf(a) >= 0 && con.indexOf(b) >= 0) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function conflict(a, b) {
+	return timeConflict(a, b) || userDefinedConflict(a, b);
+}
+
+function checkTimeConflict() {
+	var i, j, a, b;
+	for (i = 0; i<wantedCourses.length; i++) {
+		a = wantedCourses[i];
+		if (!a.conflictsWith) a.conflictsWith = [];
+		for (j = 0; j<electedCourses.length; j++) {
+			b = electedCourses[j];
+			if (a !== b && timeConflict(a, b)) {
+				a.conflictsWith.push(b);
+				console.log(a.no + " " + a.name + " conflicts with elected course " + b.no + " " + b.name);
+			}
+		}
+		for (j = i + 1; j<wantedCourses.length; j++) {
+			b = wantedCourses[j];
+			if (!b.elected && timeConflict(a, b)) {
+				a.conflictsWith.push(b);
+				console.log(a.no + " " + a.name + " conflicts with " + b.no + " " + b.name);
+			}
+		}
+	}
+}
+
+function getCredits() {
+	var i, credits = 0;
+	for (i = 0; i<electedCourses.length; i++) {
+		credits += electedCourses[i].credits;
+	}
+	return credits;
+}
+
+function checkConflict() {
+	checkTimeConflict();
+	console.log("current credits: " + getCredits());
+}
+
+var constraints = exports.constraints = [
+	function (course, toDrop) {
+		var credits = course.credits + getCredits();
+		if (credits > 32) {
+			console.log("credits exceeded 32");
+			var j = wantedCourses.indexOf(course);
+			//console.log(course.no + " " + course.name + " is not found in list, assigned highest priority by default");
+			if (j === -1) return false;
+			var c, i;
+			for (i = wantedCourses.length - 1; i>j && credits > 32; i--) {
+				c = wantedCourses[i];
+				if (c.elected) {
+					toDrop.push(c);
+					credits -= c.credits;
+				}
+			}
+			if (credits > 32) {
+				console.log("cannot find a course to drop, electing failed");
+				return false;
+			}
+		}
+		return true;
+	},
+	
+	function (course, toDrop) {
+		var i, c, p1, p2;
+		for (i = 0; i<electedCourses.length; i++) {
+			c = electedCourses[i];
+			if (conflict(course, c)) {
+				p1 = wantedCourses.indexOf(course);
+				p2 = wantedCourses.indexOf(c);
+				if (p1 !== -1 && p2 !== -1 && p1 < p2) {
+					toDrop.push(c);
+				} else {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+];
+
+pollListeners.push(function () {
+	var i, j, c, count, toDrop;
+	loop: for (i = 0; i<wantedCourses.length; i++) {
+		c = wantedCourses[i];
+		count = lessonCounts[c.id];
+		if (!c.state) resetState(c);
+		if (c.state === UNELECTED &&
+			count.sc < count.lc) {
+			toDrop = [];
+			for (j = 0; j<constraints.length; j++) {
+				if (!constraints[j](c, toDrop)) {
+					console.log("unmet constraint");
+					continue loop;
+				}
+			}
+			if (toDrop.length === 0) {
+				elect(c);
+				return;
+			} else {
+				elect(toDrop[0], true);
+				return;
+			}
+		}
+	}
+});
+
+function run(cb) {
+	function _cb() {
+		run(cb);
+	}
+	if (validate(options)) {
+		if (!loggedIn) {
+			login(_cb);
+		} else if (!courseData) {
+			loadData(_cb);
+		} else {
+			loadCounts(cb);
+		}
+	}
+}
+
+exports.run = run;
+
+var main = exports.main = function () {
+	getOptions();
+	run(function () {
+		setInterval(loadCounts, options.interval);
+	});
+}
 
 if (require.main === module) {
 	main();
